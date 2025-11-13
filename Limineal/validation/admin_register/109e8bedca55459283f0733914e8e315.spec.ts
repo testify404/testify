baefee5949c6d2cb20179b77ba99b5ff229ae07f
dharanesh <dharanesh@users.noreply.github.com>
2025-11-13 15:58:24 +0530

    test

--- Limineal/validation/admin_register/109e8bedca55459283f0733914e8e315.spec.ts
+++ Limineal/validation/admin_register/109e8bedca55459283f0733914e8e315.spec.ts
@@ -6,7 +6,7 @@ test('Generated Test', async () => {
   const executedSteps: string[] = [];
   const executionResults: any[] = [];
   const originalUserSteps: string[] = [
-    "Navigate to https://www.saucedemo.com./",
+    "Navigate to https://www.saucedemo.com/",
     "Enter \"standard_user\" in the username field with id user-name",
     "Enter \"secret_sauce\" in the password field with id password",
     "Click the Login button with id login-button"
